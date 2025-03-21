c3e1cd80381eda6890aa6a3ce0fc5d8a09cf82a3
louis <louis.niederloehner@gmail.com>
2025-03-21 11:00:01 +0100

    Use HF and Qwen + Llama ids

--- source/test.py
+++ source/test.py
@@ -1,3 +1,4 @@
+import torch
 from swift import Swift, Seq2SeqTrainer, Seq2SeqTrainingArguments
 from swift.llm import get_model_tokenizer, get_template, DatasetMeta, register_dataset, load_dataset, \
     EncodePreprocessor, PtEngine, InferRequest, RequestConfig
@@ -6,18 +7,19 @@ dataset_path = "/mnt/2tb-1/louis/data/ImageDataset/ann_new.jsonl"
 output_dir = "/home/louis/workspace/Anomaly_Det_VLM/out"
 dataset_id = "gravis-excavation"
 model_id = "deepseek-ai/deepseek-vl2-tiny"
-
+# model_id = "Qwen/Qwen2.5-VL-3B-Instruct"
+# model_id = "meta-llama/Llama-3.2-1B-Instruct"
 
 def train():
     num_proc = 1
     data_seed = 42
 
     # Retrieve the model and template, and add a trainable LoRA module
-    model, tokenizer = get_model_tokenizer(model_id)
+    model, tokenizer = get_model_tokenizer(model_id, use_hf=True)
     template = get_template(model.model_meta.template, tokenizer, ...)
 
     # Download and load the dataset, and encode the text into tokens
-    train_dataset, val_dataset = load_dataset(dataset_id)
+    train_dataset, val_dataset = load_dataset(dataset_path)
     train_dataset = EncodePreprocessor(template=template)(train_dataset, num_proc=num_proc)
     val_dataset = EncodePreprocessor(template=template)(val_dataset, num_proc=num_proc)
 
@@ -42,7 +44,7 @@ def train():
         save_total_limit=5,
         logging_steps=5,
         dataloader_num_workers=1,
-        data_seed=data_seed,
+        data_seed=data_seed
     )
 
     # Train the model
@@ -58,7 +60,7 @@ def train():
 
 
 def infer():
-    max_new_tokens = 512
+    max_new_tokens = 2048
     temperature = 0
     # Perform inference using the native PyTorch engine
     engine = PtEngine(model_id, max_batch_size=2, use_hf=True)
@@ -67,7 +69,7 @@ def infer():
     messages = [{"role": "system", "content": "You are a professional anomaly detection and classification tool that detects objects that could prevent an excavator from digging."},
                 {"role": "user", "content": "<image>This is an image of a trench that has been dug by an excavator. Does the trench contain any objects that could hinder excavation? Common examples of anomalies are pipes, cables, wires, tools, large stones and wooden planks. Provide only the english names of the objects that you detect in the trench as a list separated by commas. If you only see objects like a trench, dirt, gravel, part of an excavator or a whole excavator, you ignore them and return an empty list: []"}]
 
-    images = ["/home/louis/workspace/Anomaly_Det_VLM/custom/rock.png"]
+    images = ["/home/louis/workspace/Anomaly_Det_VLM/custom/wire1.png"]
 
     infer_request = InferRequest(messages=messages, images=images)
 
@@ -85,4 +87,5 @@ if __name__ == "__main__":
 
     dataset = load_dataset(dataset_path)  # Load dataset using its path
     print(dataset[0])  # Check first example
-    infer()
+    # infer()
+    train()
